923b8816e000d915c584c3e95bfc5ff5382e36c9
Pierre Wizla <pwizla+github@gmail.com>
2015-07-27 00:41:44 +0200

    Add Shift-Click behavior (delete completed item)

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ $(document).ready( function() {
   // === END OF FUNCTION HANDLING NEW TASKS ===
 
 
-
   /* The little function below toggles 'itemdone' class 
      on items (from the '.list') that are clicked
      so that the item is greyed out and formatted as strikethrough;
@@ -150,11 +149,16 @@ $(document).ready( function() {
 
 
   /* The little function below does the opposite
-     e.g. moving back a "done" item to the "todo" list when clicked */
-  $(document).on('click', '.itemdone', function() {
-      $(this).removeClass('itemdone');
-      // moves back the undone item to the 'list' div
-      $('.list').append(this);
+     e.g. moving back a "done" item to the "todo" list when clicked.
+     IF SHIFT CLICK, item is DELETED. */
+  $(document).on('click', '.itemdone', function(e) {
+      if (e.shiftKey) { // if Shift-Click instead of a simple Click
+        $(this).remove(); // individual item is deleted
+      } else {
+        $(this).removeClass('itemdone');
+        // moves back the undone item to the 'list' div
+        $('.list').append(this);
+      }
       // reflects changes to $list and $listdone
       updateTodoList();
       updateDoneList();
